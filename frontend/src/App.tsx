2a525fd96a74e6dc2dd4e380e7bcbda5487adec8
abhi-swami <manjuswami338@gmail.com>
2023-05-12 19:55:25 +0530

    chart added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import Allroutes from './Pages/Allroutes';
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
+    
       <Allroutes/>
       <Footer/>
     </div>
