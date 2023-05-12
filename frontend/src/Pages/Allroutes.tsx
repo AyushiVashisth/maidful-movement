5bf8003bcfcc1e177693a3c93467421170ddd4bb
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-05-12 11:16:45 +0530

    home

---
+++ frontend/src/Pages/Allroutes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import BMI from "./BMI";
+import LandingPage from "./Home";
+
+const Allroutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/bmi" element={<BMI />} />
+      </Routes>
+    </div>
+  );
+};
+export default Allroutes;
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -16,31 +16,6 @@ const LandingPage: React.FC = () => {
   const cancelButtonRef = useRef(null);
   return (
     <div className="bg-gray-50">
-      {/* Hero */}
-      {/* <section className="bg-gradient-to-r from-blue-600 to-purple-500 py-16">
-                                <div className="max-w-7xl mx-auto px-4">
-                                        <div className="flex flex-col md:flex-row md:items-center">
-                                                <div className="md:w-1/2 mb-10 md:mb-0">
-                                                        <h1 className="text-4xl font-bold text-white mb-4">
-                                                                Your Health<span className="text-blue-300">Matters</span>
-                                                        </h1>
-                                                        <p className="text-white leading-relaxed">
-                                                                At HealthPlus, we care about your wellbeing. Our team of professionals are dedicated to
-                                                                providing the highest level of care and attention to ensure that you lead a healthy and
-                                                                fulfilling life.
-                                                        </p>
-                                                        <div className="mt-6">
-                                                                <button className="bg-blue-800 text-white py-2 px-4 rounded-full hover:bg-blue-700">
-                                                                        Learn More
-                                                                </button>
-                                                        </div>
-                                                </div>
-                                                <div className="md:w-1/2">
-                                                        <img src="/assets/images/hero.png" alt="Hero" />
-                                                </div>
-                                        </div>
-                                </div>
-                        </section> */}
       <div className="max-w-7xl mx-auto px-2">
         <div className="w-300px justify-center items-center mb-4">
           <img
@@ -229,7 +204,7 @@ const LandingPage: React.FC = () => {
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                       >
                         <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg p-6">
-                          <p className="mt-4">
+                          <p className="mt-4 font-bold">
                             How does keto diet affect your body?
                           </p>
                           <div className="bg-gray-200 p-6">
@@ -254,58 +229,62 @@ const LandingPage: React.FC = () => {
                               muscle and improving insulin sensitivity
                             </p>
                           </div>
-                          <p className="mt-4">1. Follow portion control</p>
+                          <h5 className="mt-4 font-bold">Types of Keto Diet</h5>
+                          <p className="mt-4">1. Standard Ketogenic Diet</p>
                           <div className="bg-gray-200 p-6">
                             <p>
-                              As losing weight is all about keeping a tab on
-                              your calorie intake, ensuring portion control can
-                              go a long way not only to eat healthy but also to
-                              manage your weight.
-                            </p>
-                            <p>
-                              To prevent eating in excess, you can switch to
-                              smaller plates, spoons, and bowls which not only
-                              cause you to eat less but also reduce your overall
-                              intake of food. The same rule applies if you are
-                              eating out or ordering out.
+                              An eating plan which is made of 75% fat, 20%
+                              protein and only 5% carbohydrates.
                             </p>
                           </div>
-                          <p className="mt-4">2. Practice mindful eating</p>
+                          <p className="mt-4">
+                            2. Very Low Carb Ketogenic Diet:
+                          </p>
                           <div className="bg-gray-200 p-6">
                             <p>
-                              Our busy schedule calls for a quick meal and
-                              instant snacks which can impact our weight loss
-                              and overall health. This is why it is important to
-                              not only eat a healthy diet but also savor each
-                              bite and enjoy the food you eat. Chew your food
-                              slowly and avoid distractions like TV or phones
-                              when eating. This is because, when you are
-                              stressed or distracted you tend to eat a lot which
-                              can affect your weight loss plan.{" "}
+                              A diet with no more than 50g of carbohydrates per
+                              day.
                             </p>
                           </div>
-                          <p className="mt-4">3. Maintain a diet journal </p>
+                          <p className="mt-4">3.MCT Ketogenic Diet: </p>
                           <div className="bg-gray-200 p-6">
                             <p>
-                              When losing weight, it is mandatory to keep a
-                              close tab on things that favor your weight loss
-                              goals and those that hinder your plan. For
-                              example, there are times when you strictly follow
-                              your diet throughout the day but eat a processed
-                              snack to satiate your cravings. These small things
-                              are often neglected which in turn can hinder your
-                              weight loss. So jotting down your daily routine
-                              such as your calorie intake, the quantity of food
-                              eaten, types of foods eaten, cheat meals (if any),
-                              water intake, etc. By doing so, it will be easy to
-                              work on your problem areas & achieve your weight
-                              loss goal.
+                              Also known as Medium Chain Triglyceride Diet, it
+                              is not based on the division of diet ratios.
+                              Instead, it uses MCT oil in food, namely, coconut
+                              oil for the production of ketone bodies.
+                            </p>
+                          </div>
+                          <p className="mt-4">4.Cyclical Ketogenic Diet: </p>
+                          <div className="bg-gray-200 p-6">
+                            <p>
+                              It is a rotation between high-carb diet day to
+                              low-carb diet day in order to bring the body out
+                              of ketosis.
+                            </p>
+                          </div>
+                          <p className="mt-4">5.Targeted Ketogenic Diet:</p>
+                          <div className="bg-gray-200 p-6">
+                            <p>
+                              In this, adding of carbohydrates is allowed around
+                              the workout hours.
+                            </p>
+                          </div>
+                          <p className="mt-4 font-bold">Is keto diet safe?</p>
+                          <div className="bg-gray-200 p-6">
+                            <p>
+                              Using keto diet for weight loss can be safe only
+                              if you follow it under expert supervision. Keto
+                              diet requires to be handled with close supervision
+                              and care because you may need timely check-ups of
+                              your blood parameters since it involves high fat,
+                              minimal protein and very low carb intake.
                             </p>
                           </div>
                           <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                               type="button"
-                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-bold  font-bold text-gray-900 shadow-lg ring-1 ring-inset ring-gray-300 hover:bg-gray-300 sm:mt-0 sm:w-auto"
                               onClick={() => setOpen2(false)}
                               ref={cancelButtonRef}
                             >
@@ -325,7 +304,8 @@ const LandingPage: React.FC = () => {
       <div className="max-w-7xl mx-auto px-2">
         <div className="max-w-2xl justify-center items-center mb-4 mx-auto">
           <img
-            src="https://images.theconversation.com/files/475914/original/file-20220725-15-unq819.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop" alt="blog"
+            src="https://images.theconversation.com/files/475914/original/file-20220725-15-unq819.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop"
+            alt="blog"
           />
         </div>
 
@@ -339,7 +319,8 @@ const LandingPage: React.FC = () => {
                 5 Simple Ways to Improve Your Mental Health
               </h1>
               <p className="mt-4">
-              Taking care of your mental health is just as important as taking care of your physical health
+                Taking care of your mental health is just as important as taking
+                care of your physical health
               </p>
               <Transition.Root show={open3} as={Fragment}>
                 <Dialog
@@ -373,39 +354,58 @@ const LandingPage: React.FC = () => {
                       >
                         <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg p-6">
                           <h5 className="text-3xl font-bold  uppercase">
-                          Here are 5 simple ways you can improve your mental health:
+                            Here are 5 simple ways you can improve your mental
+                            health:
                           </h5>
                           <div className="bg-gray-200 p-6 ">
                             <p>
-                            1. Practice Mindfulness: Mindfulness is the practice of being present in the moment and focusing on your thoughts and feelings without judgment. It can help reduce stress and anxiety and improve overall mental health.
+                              1. Practice Mindfulness: Mindfulness is the
+                              practice of being present in the moment and
+                              focusing on your thoughts and feelings without
+                              judgment. It can help reduce stress and anxiety
+                              and improve overall mental health.
                             </p>
                           </div>
                           <div className="bg-gray-200 p-6 t-2">
                             <p>
-                            2. Exercise Regularly: Exercise is not only good for your physical health but also your mental health. It can help reduce stress and anxiety and improve mood and self-esteem.
+                              2. Exercise Regularly: Exercise is not only good
+                              for your physical health but also your mental
+                              health. It can help reduce stress and anxiety and
+                              improve mood and self-esteem.
                             </p>
                           </div>
                           <div className="bg-gray-200 p-6">
                             <p>
-                              3. Get Enough Sleep: Getting enough sleep is essential for good mental health. Lack of sleep can lead to increased stress and anxiety, and can also affect mood and cognitive function.
+                              3. Get Enough Sleep: Getting enough sleep is
+                              essential for good mental health. Lack of sleep
+                              can lead to increased stress and anxiety, and can
+                              also affect mood and cognitive function.
                             </p>
                           </div>
                           <div className="bg-gray-200 p-6">
                             <p>
-                              4. Connect with Others: Social connection is important for mental health. Make time to connect with friends and family, or join a community group or club to meet new people.
+                              4. Connect with Others: Social connection is
+                              important for mental health. Make time to connect
+                              with friends and family, or join a community group
+                              or club to meet new people.
                             </p>
                           </div>
                           <div className="bg-gray-200 p-6">
                             <p>
-                              5. Practice Self-Care: Taking care of yourself is essential for good mental health. This can include activities like taking a relaxing bath, reading a book, or going for a walk.
+                              5. Practice Self-Care: Taking care of yourself is
+                              essential for good mental health. This can include
+                              activities like taking a relaxing bath, reading a
+                              book, or going for a walk.
                             </p>
                           </div>
                           <div className="p-6">
                             <p>
-                            By incorporating these simple practices into your daily routine, you can help improve your mental health and overall well-being.
+                              By incorporating these simple practices into your
+                              daily routine, you can help improve your mental
+                              health and overall well-being.
                             </p>
                           </div>
-                          
+
                           <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                               type="button"
