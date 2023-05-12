38731b38974439d5fb3ecafea2fa7d0703e63457
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-05-12 23:18:34 +0530

    slider in landing

--- frontend/src/Pages/Allroutes.tsx
+++ frontend/src/Pages/Allroutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import BMI from "./BMI";
 import LandingPage from "./Home";
+// import LandingPage from "./Home";
 
 const Allroutes = () => {
   return (
